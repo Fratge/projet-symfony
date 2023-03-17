a9e8e4e408fdeb440e5cae9fc55eff36136a2d6b
Fratge <114362943+Fratge@users.noreply.github.com>
2023-03-17 15:33:31 +0100

    update: Chaussure CRUD

--- src/Controller/StaticPages.php
+++ src/Controller/StaticPages.php
@@ -34,49 +34,4 @@ class StaticPages extends AbstractController
         ]);
     }
 
-    /**
-    * @Route("/chaussures", name="chaussures")
-    */
-    public function chaussures(): Response
-    {
-        // Récupère le dépôt lié à la classe Castle
-        $repo = $this->getDoctrine()
-            ->getRepository(Chaussure::class);
-    
-        // Exécute une requête SELECT
-        $chaussures = $repo->findAll();
-        $titlePage = 'test';
-    
-        // Utilisation du résultat
-        return $this->render('chaussures.html.twig', [
-            'chaussures' => $chaussures,
-            'titlePage' => $titlePage,
-        ]);
-    }
-
-    /**
-     * @Route("/chaussure/{id}", name="chaussure_readone")
-     * @param int $id
-     * @return Response
-    */
-    public function chaussure(int $id): Response
-    {   
-        $titlePage = 'test';
-
-        $repo = $this->getDoctrine()
-            ->getRepository(Chaussure::class);
-    
-        $chaussures = $repo->findAll();
-        $chaussure  = $repo->find($id);
-    
-        // si le château recherché n'existe pas, redirection vers la route "chateaux"
-        if (!$chaussure) {
-            return $this->redirectToRoute('chaussures');
-        }
-    
-        return $this->render('chaussure.html.twig', [
-            'chaussures' => $chaussures,
-            'chaussure' => $chaussure
-        ]);
-    }
 }
